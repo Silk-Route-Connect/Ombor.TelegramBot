6234dbefcfd440609753963d797acbe2d4443cce
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2025-08-11 16:45:00 +0500

    Create GetProductsRequest.cs

---
+++ Ombor.TelegramBot.Domain/Entities/GetProductsRequest.cs
@@ -0,0 +1,12 @@
+﻿using Ombor.TelegramBot.Domain.Enums;
+
+namespace Ombor.TelegramBot.Domain.Entities;
+
+public class GetProductsRequest
+{
+    public string? SearchTerm { get; set; }
+    public int? CategoryId { get; set; }
+    public decimal? MinPrice { get; set; }
+    public decimal? MaxPrice { get; set; }
+    public ProductType? Type { get; set; }
+}
