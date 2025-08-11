efe9c4531fe5e62327738f4629e953ef7c4e3437
Boburmirzo <137428656+Boburm1rzo@users.noreply.github.com>
2025-08-11 16:43:22 +0500

    Create DependencyInjection.cs

---
+++ Ombor.TelegramBot.Application/Extentions/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Ombor.TelegramBot.Application.Interfaces;
+using Ombor.TelegramBot.Application.Services;
+
+namespace Ombor.TelegramBot.Application.Extentions;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddSingleton<HttpClient>();
+        services.AddScoped<IApiService, ApiService>();
+
+        return services;
+    }
+}
